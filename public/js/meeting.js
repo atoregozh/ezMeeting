40a6c8dba75672c18608465b23f45b09658575eb
Kesiena Owho-Ovuakporie <kesiena115@gmail.com>
2016-01-15 12:07:40 -0500

    Adds event handlers to the grid side buttons and refactors the event handlers for the grid scrolling

--- public/js/meeting.js
+++ public/js/meeting.js
@@ -28,29 +28,76 @@ $(document).ready(function(){
 		console.log('clicked dp2');
 	}).data('datepicker');
 
-	$('#br').on('scroll', function (e) {
-	    $('#tr').scrollLeft($(this).scrollLeft());
-	    e.stopPropagation();
-	    e.preventDefault();
-	});
 
 	$('#tr').on('scroll', function (e) {
-	    $('#br').scrollLeft($(this).scrollLeft());
-	    e.stopPropagation();
+		e.stopPropagation();
 	    e.preventDefault();
+	    // If scrolled horizontally, simply scroll the bottom right panel and allow 
+	    // the bottom right's handler to perform all the fancy stuff.
+	    $('#br').scrollLeft($(this).scrollLeft());
 	});
 
 	$('#bl').on('scroll', function (e) {
-	    $('#br').scrollTop($(this).scrollTop());
-	    e.stopPropagation();
+		e.stopPropagation();
 	    e.preventDefault();
+	    // If scrolled vertically, simply scroll the bottom right panel and allow 
+	    // the bottom right's handler to perform all the fancy stuff.
+	    $('#br').scrollTop($(this).scrollTop());
 	});
 
 	$('#br').on('scroll', function (e) {
-	    $('#bl').scrollTop($(this).scrollTop());
-	    e.stopPropagation();
+		e.stopPropagation();
 	    e.preventDefault();
+
+	    // Vertical scrolling
+	    $('#bl').scrollTop($(this).scrollTop());
+
+	    // Horizontal scrolling
+	    // If the event originated in this item, the top right panel will be scrolled to the same location.
+	    var left = $(this).scrollLeft();
+	    $('#tr').scrollLeft(left);
+	    
+		var threshold = 50;
+	    if((left + $(this).innerWidth()) >= ($(this)[0].scrollWidth - threshold)) {
+	    	// Enable right download
+            var rightBtn = '.grid-side-btn.right';
+		    if(!$(rightBtn).hasClass('active')) {
+		    	$(rightBtn).addClass('active');
+		    }
+        }
+        else if(left < threshold) {
+        	// Enable left download
+        	var leftBtn = '.grid-side-btn.left';
+			if(!$(leftBtn).hasClass('active')) {
+				$(leftBtn).addClass('active');
+			}
+        }else {
+        	// Disable both left and right download
+        	$('.grid-side-btn').removeClass('active');
+        }
+	});
+
+	$('.grid-side-btn.left').click(function() {
+		$('#br').scrollLeft($('#br').scrollLeft() - 101); // 101 corresponds to the width of each column
+		if($(this).hasClass('active')) {
+			downloadEarlierDays();
+		}
+	});
+
+	$('.grid-side-btn.right').click(function() {
+		$('#br').scrollLeft($('#br').scrollLeft() + 101); // 101 corresponds to the width of each column
+		if($(this).hasClass('active')) {
+			downloadLaterDays();
+		}
 	});
 
 	// ~~~~~ End initialize .date-input ~~~~~
 });
+
+function downloadEarlierDays() {
+	console.log('>>> TODO: implement downloadEarlierDays()');
+}
+
+function downloadLaterDays() {
+	console.log('>>> TODO: implement downloadLaterDays()');
+}
