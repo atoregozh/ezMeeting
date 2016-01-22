3589cb27b48a8150c486f13186e3611cb3755e30
Kesiena Owho-Ovuakporie <kesiena115@gmail.com>
2016-01-21 20:51:32 -0500

    Edits the notifications layout

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var passport = require('passport');
-var flash    = require('connect-flash');
 
 var path = require('path');
 var favicon = require('serve-favicon');
